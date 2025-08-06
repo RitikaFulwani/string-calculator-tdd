07bb6762ee6c1ef929923389b5bdba0066e01bff
Ritika Fulwani <117587348+RitikaFulwani@users.noreply.github.com>
2025-08-06 20:32:39 +0530

    Support newlines as delimiters

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -1,4 +1,4 @@
-test('returns sum of any amount of numbers', () => {
-    expect(add("1,2,3")).toBe(6);
-    expect(add("1,2,3,4,5")).toBe(15);
+test('handles newlines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("1\n2\n3")).toBe(6);
 });
